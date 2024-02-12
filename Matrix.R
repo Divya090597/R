643bd976c132ba94d4e9cd8c6e15451cce92aa33
Divya090597 <156469276+Divya090597@users.noreply.github.com>
2024-02-11 22:02:37 -0500

    Add files via upload

---
+++ Matrix.R
@@ -0,0 +1,19 @@
+A <- matrix(1:100, nrow=10)
+A
+
+det_A <- det(A)
+det_A
+
+A_inverse <- solve(A)
+A_inverse
+
+B <- matrix(1:1000, nrow=10)
+B
+
+det_B <- det(B)
+det_B
+
+B_inverse <- solve(B)
+B_inverse
+
+
