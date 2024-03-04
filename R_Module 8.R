c675fceee31ba84a43cabaed8e60a5f3bc838fd1
Divya090597 <156469276+Divya090597@users.noreply.github.com>
2024-03-03 20:47:20 -0500

    Add files via upload

---
+++ R_Module 8.R
@@ -0,0 +1,23 @@
+# Load required packages
+install.packages("plyr")
+library(plyr)
+
+# Import dataset into R
+x <- read.table(file.choose(), header = TRUE, sep = ",")
+x
+
+#run plyr generates for the mean of both Age and Grade split by gender
+y = ddply(x,"Sex",transform, Grade.Average=mean(Grade))
+y
+
+#Print this to a file
+write.table(y,"Sorted_Average")
+
+#Generate a CSV
+write.table(y,"Sorted_Average",sep=",") 
+
+#Filter the names in the given list that contain the letter i.
+newx = subset(x,grepl("[iI]",x$Name))
+
+#write this subset to a file
+write.table(newx,"DataSubset",sep=",") 
